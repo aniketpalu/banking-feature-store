25727b547b1cdd02a8f6a04eb6dcc64df48f8b93
Aniket Paluskar <apaluska@redhat.com>
2025-11-26 18:19:11 +0530

    Updated permissions
    Signed-off-by: Aniket Paluskar <apaluska@redhat.com>

--- feature_repo/permissions.py
+++ feature_repo/permissions.py
@@ -7,7 +7,8 @@ from feast.permissions.action import READ, AuthzedAction, ALL_ACTIONS
 from feast.permissions.permission import Permission
 from feast.permissions.policy import (
     GroupBasedPolicy,
-    CombinedGroupNamespacePolicy
+    CombinedGroupNamespacePolicy,
+    NamespaceBasedPolicy
 )
 from feast.project import Project
 from feast.data_source import DataSource
@@ -24,7 +25,7 @@ data_team_groups = ["data-engineers"]
 ds_team_groups = ["ds-team"]
 
 # # Simple namespace definitions
-prod_namespaces = ["feast-eap"]
+prod_namespaces = ["feast"]
 staging_namespaces = ["feast-staging"]
 
 
@@ -41,20 +42,20 @@ resource_types_without_permissions = [
 admin_perm = Permission(
     name="admin_permissions",
     types=ALL_RESOURCE_TYPES,
-    policy=GroupBasedPolicy(groups=admin_groups),
+    policy=NamespaceBasedPolicy(namespaces=prod_namespaces),
     actions=ALL_ACTIONS
 )
 
-data_team_perm = Permission(
-    name="data_team_permissions",
-    types=resource_types_without_permissions,
-    policy=CombinedGroupNamespacePolicy(groups=data_team_groups, namespaces=prod_namespaces),
-    actions=ALL_ACTIONS
-)
+# data_team_perm = Permission(
+#     name="data_team_permissions",
+#     types=resource_types_without_permissions,
+#     policy=CombinedGroupNamespacePolicy(groups=data_team_groups, namespaces=prod_namespaces),
+#     actions=ALL_ACTIONS
+# )
 
-ds_team_perm = Permission(
-    name="ds_team_permissions",
-    types=resource_types_without_permissions,
-    policy=CombinedGroupNamespacePolicy(namespaces=staging_namespaces, groups=ds_team_groups),
-    actions=[AuthzedAction.DESCRIBE] + READ
-)
+# ds_team_perm = Permission(
+#     name="ds_team_permissions",
+#     types=resource_types_without_permissions,
+#     policy=CombinedGroupNamespacePolicy(namespaces=staging_namespaces, groups=ds_team_groups),
+#     actions=[AuthzedAction.DESCRIBE] + READ
+# )
